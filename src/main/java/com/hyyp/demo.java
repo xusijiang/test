1da7fb5dab5f97b39691c295d54d87cdd2b0d26d
andxu <1533312041@qq.com>
2019-12-01 19:34:07 +0800

    init

---
+++ src/main/java/com/hyyp/demo.java
@@ -0,0 +1,10 @@
+package com.hyyp;
+
+/**
+ * Created by AndXu on 2019/14/1
+ */
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("hello word!");
+    }
+}
