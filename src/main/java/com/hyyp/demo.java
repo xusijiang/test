bd8effcc9d5df127fae59a9fa26e55b1f5f0e2fb
andxu <1533312041@qq.com>
2019-12-01 20:53:34 +0800

    新增sout("kkkk")

--- src/main/java/com/hyyp/demo.java
+++ src/main/java/com/hyyp/demo.java
@@ -7,5 +7,6 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("hello word!");
         System.out.println("hello git !");
+        System.out.println("kkkkk");
     }
 }
